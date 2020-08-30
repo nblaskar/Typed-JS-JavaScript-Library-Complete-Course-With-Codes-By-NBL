d68ce2bc493e85bdae16254f14dc1c4a871f4d8c
NUR BAHAR LASKAR <42918029+nblaskar@users.noreply.github.com>
2020-08-30 21:11:39 +0530

    Add files via upload

---
+++ TypedJS Project/custom.js
@@ -0,0 +1,6 @@
+let spantyped = new Typed("#spanid", {
+  strings: ["Developer", "Designer", "Learner", "Hacker"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
---
+++ custom.js
@@ -0,0 +1,80 @@
+/*
+//Example-1
+let example1 = new Typed("#example1", {
+  stringsElement: "#mydivid",
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+//Example-2
+let example2 = new Typed("#example2", {
+  strings: ["Example 2 Animation", "Welcome"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+//Example-3
+let example3 = new Typed("#example3", {
+  strings: ["Example 3"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+//Example-4
+let example3 = new Typed("#example4", {
+  strings: ["Developer", "Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  // fadeOut: true,
+  // fadeOutDelay: 2000,
+  cursorChar: "*",
+  startDelay: 2000,
+  backDelay: 2000,
+  showCursor: true,
+  shuffle: true,
+  smartBackspace: true,
+});
+
+
+//Example-5
+let example5 = new Typed("#example5", {
+  strings: ["You can write", "Your Name"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  attr: "placeholder",
+});
+
+//Example-6
+let example6 = new Typed("#example6", {
+  strings: ["You can write ^3000", "Your Name"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+//Example-7
+let example7 = new Typed("#example7", {
+  //==>Process-1
+  //strings: ["I AM <br>Developer<br>Designer"],
+
+  //==>Process-2
+  // strings: ["I AM ^3000 <br>Developer<br>Designer"],
+
+  //==>Process-3
+  strings: ["I AM ^3000 `<br>Developer``<br>Designer`"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+*/
+//Example-8
+let example8 = new Typed("#example8", {
+  strings: ["I am Developer", "Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
